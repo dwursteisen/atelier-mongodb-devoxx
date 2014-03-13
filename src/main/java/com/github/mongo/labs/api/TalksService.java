5053a80b79739546319251be0dd292e73b0c1454
David Wursteisen <david.wursteisen+github@gmail.com>
2014-03-13 16:24:23 +0100

    Update des copyrights

--- src/main/java/com/github/mongo/labs/api/TalksService.java
+++ src/main/java/com/github/mongo/labs/api/TalksService.java
@@ -1,7 +1,5 @@
 package com.github.mongo.labs.api;
 
-import java.net.UnknownHostException;
-
 import javax.annotation.PostConstruct;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,17 +7,26 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import org.jongo.Jongo;
-import org.jongo.MongoCollection;
+import java.net.UnknownHostException;
 
 import com.github.mongo.labs.model.Talk;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
 
 /**
- * Created with IntelliJ IDEA. User: david.wursteisen Date: 13/03/14 Time: 13:48 To change this template use File | Settings | File
- * Templates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 @Path("/talks")
 public class TalksService {
