3e70135a8dc2dec744e777c6c41bfd3522e034ec
David Wursteisen <david.wursteisen+git@gmail.com>
2014-03-27 13:06:38 +0100

    Fulltext search

--- src/main/java/com/github/mongo/labs/api/SearchService.java
+++ src/main/java/com/github/mongo/labs/api/SearchService.java
@@ -15,15 +15,23 @@
 package com.github.mongo.labs.api;
 
 import com.github.mongo.labs.model.Talk;
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(value = "/search", description = "Recherche fulltext")
 @Path("/search")
@@ -31,14 +39,43 @@ import javax.ws.rs.core.MediaType;
 @Singleton
 public class SearchService {
 
+    private MongoCollection collection;
+
+    @PostConstruct
+    public void init() throws UnknownHostException {
+        DB db = new MongoClient("localhost").getDB("devoxx");
+        collection = new Jongo(db).getCollection("$cmd");
+    }
+
     @GET
     @Path("/{term}")
-    @ApiOperation(value = "Recherche les talks ayant le terme fourni en paramètre. " +
+    @ApiOperation(value = "Recherche les talks ayant le terme fourni en paramètre (ex: java). " +
             "Cette recherche utilise la fonction FullText Search de MongoDB. ",
             notes = "Il ne faut pas oublier d'activer cette fonctionnalité sur mongo : " +
                     "<b>mongod --setParameter textSearchEnabled=true</b>"
     )
     public Iterable<Talk> search(@PathParam("term") String term) {
-        return null;
+        Output output = collection.findOne("{text: 'talks', search: #, limit: 5}", term).as(Output.class);
+        List<Talk> result = new ArrayList<Talk>();
+
+        if (output == null) {
+            return result;
+        }
+
+        for (Result r : output.results) {
+            result.add(r.obj);
+        }
+        return result;
+    }
+
+
+    public static class Output {
+        private Iterable<Result> results;
+
+    }
+
+    public static class Result {
+        public double score;
+        public Talk obj;
     }
 }
