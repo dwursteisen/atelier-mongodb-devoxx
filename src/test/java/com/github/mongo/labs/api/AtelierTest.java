495e25c1c0850091a0cb2b59f565bbecde26c2f1
karesti-fcms <karesti@figarocms.fr>
2014-04-15 17:40:08 +0200

    tear down data on speakers test

--- src/test/java/com/github/mongo/labs/api/AtelierTest.java
+++ src/test/java/com/github/mongo/labs/api/AtelierTest.java
@@ -2,6 +2,8 @@ package com.github.mongo.labs.api;
 
 import java.net.UnknownHostException;
 
+import org.jongo.Jongo;
+import org.junit.Before;
 import org.junit.BeforeClass;
 
 import com.mongodb.DB;
@@ -11,13 +13,18 @@ public class AtelierTest {
 
 	static DB db;
 
+    static Jongo jongo;
+
 	@BeforeClass
 	public static void getDB() {
 
 		try {
 			db = new MongoClient("localhost").getDB("devoxx");
+            jongo = new Jongo(db);
 		} catch (UnknownHostException e) {
 			System.out.println("La base de données devoxx n'existe pas. Mongo est up ? ");
+            e.printStackTrace();
 		}
 	}
+
 }
